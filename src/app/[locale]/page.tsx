91e4cd46f8fe280d33a4ce3947d1b4f422f5a34f
oleg.pronchenko <traeop@gmail.com>
2025-10-09 12:25:25 +0300

    react-bootstrap-icons setup is complete

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,10 @@
+import { Circle } from "react-bootstrap-icons";
+
 export default function Home() {
-  return <h1>home</h1>;
+  return (
+    <>
+      <h1>home</h1>
+      <Circle size={30} color="green" />
+    </>
+  );
 }
