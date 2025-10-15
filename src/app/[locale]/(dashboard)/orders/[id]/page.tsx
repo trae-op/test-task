2697fb3d5e011dd7a72128359028dd289abd9f34
oleg.pronchenko <traeop@gmail.com>
2025-10-15 16:01:47 +0300

    A new DetailEntityLoading component for reuse

--- src/app/[locale]/(dashboard)/orders/[id]/page.tsx
+++ src/app/[locale]/(dashboard)/orders/[id]/page.tsx
@@ -2,5 +2,6 @@ import type { TPageProps } from './types';
 
 export default async function OrderPage({ params }: TPageProps) {
 	const { id } = await params;
+
 	return <div>Order Page: {id}</div>;
 }
---
+++ src/components/ui/DetailEntityLoading/DetailEntityLoading.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import clsx from 'clsx';
+import { Placeholder } from 'react-bootstrap';
+
+import styles from './DetailEntityLoading.module.scss';
+
+const BLOCK = 'container-loading';
+
+export const DetailEntityLoading = () => {
+	return (
+		<div className={clsx(styles[BLOCK], 'w-100 d-flex gap-2 my-5')}>
+			<Placeholder animation='glow' className='w-100'>
+				<Placeholder xs={5} className='w-100 h-100' />
+			</Placeholder>
+
+			<Placeholder animation='glow' className='w-100'>
+				<Placeholder xs={7} className='w-100 h-100' />
+			</Placeholder>
+		</div>
+	);
+};
