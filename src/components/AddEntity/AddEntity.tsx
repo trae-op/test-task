3962e890ab1d59d2018d31f3dff9062715c100be
oleg.pronchenko <traeop@gmail.com>
2025-10-17 17:22:21 +0300

    implemented Popup to AddEntity because it must be popup

--- src/components/AddEntity/AddEntity.tsx
+++ src/components/AddEntity/AddEntity.tsx
@@ -1,18 +1,29 @@
+'use client';
+
 import { memo } from 'react';
 
 import { AddEntityButton } from '@/components/ui/AddEntityButton';
+import { Popup } from '@/components/ui/Popup';
 
 import styles from './AddEntity.module.scss';
 import type { TAddEntityProps } from './types';
 
 const BLOCK = 'add-entity';
 
-export const AddEntity = memo((props: TAddEntityProps) => {
-	const { title, totalValue } = props;
-
+export const AddEntity = memo(({ title, totalValue }: TAddEntityProps) => {
 	return (
 		<div className={styles[BLOCK]}>
-			<AddEntityButton aria-label='add content' />
+			<Popup
+				componentButton={AddEntityButton}
+				title={'Add this order?'}
+				confirmText={'Add'}
+				cancelText={'Cancel'}
+				applyButtonClassName=''
+				onApply={onClose => {
+					console.log('add order');
+					onClose();
+				}}
+			/>
 
 			<div className={styles[`${BLOCK}__text`]}>
 				<span>{title}</span>
