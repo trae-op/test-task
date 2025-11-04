2430213821df537ab60aee639033c96308bb3709
oleg.pronchenko <traeop@gmail.com>
2025-11-04 16:53:20 +0200

    refactor(SelectField): memoize component and render provided labels instead of translating

--- src/components/SelectField/SelectField.tsx
+++ src/components/SelectField/SelectField.tsx
@@ -1,49 +1,49 @@
 import clsx from 'clsx';
-import { useTranslations } from 'next-intl';
+import { memo } from 'react';
 import { Form } from 'react-bootstrap';
 
 import styles from './SelectField.module.scss';
 import type { SelectProps } from './types';
 
-export const SelectField = ({
-	options,
-	value,
-	onChange,
-	placeholder,
-	className,
-	disabledOptions
-}: SelectProps) => {
-	const t = useTranslations('App');
-
-	return (
-		<Form.Select
-			value={value}
-			onChange={onChange}
-			className={clsx(styles['select-field'], className)}
-		>
-			{placeholder && (
-				<option value='' disabled hidden>
-					{placeholder}
-				</option>
-			)}
-			{options.map(option => (
-				<option
-					key={option.value}
-					disabled={
-						disabledOptions
-							? disabledOptions.some(
-									disabled =>
-										(typeof disabled === 'object'
-											? disabled.value
-											: disabled) === option.value
-								)
-							: undefined
-					}
-					value={option.value}
-				>
-					{t(option.label)}
-				</option>
-			))}
-		</Form.Select>
-	);
-};
+export const SelectField = memo(
+	({
+		options,
+		value,
+		onChange,
+		placeholder,
+		className,
+		disabledOptions
+	}: SelectProps) => {
+		return (
+			<Form.Select
+				value={value}
+				onChange={onChange}
+				className={clsx(styles['select-field'], className)}
+			>
+				{placeholder && (
+					<option value='' disabled hidden>
+						{placeholder}
+					</option>
+				)}
+				{options.map(option => (
+					<option
+						key={option.value}
+						disabled={
+							disabledOptions
+								? disabledOptions.some(
+										disabled =>
+											(typeof disabled === 'object'
+												? disabled.value
+												: disabled) === option.value
+									)
+								: undefined
+						}
+						value={option.value}
+					>
+						{option.label}
+					</option>
+				))}
+			</Form.Select>
+		);
+	}
+);
