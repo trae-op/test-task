4cd3a76769a84bcf10f10aab046f67ec22884324
oleg.pronchenko <traeop@gmail.com>
2025-11-05 16:16:50 +0200

    chore: commit workspace changes (assistant)

--- src/components/MultiSelectField/MultiSelectField.tsx
+++ src/components/MultiSelectField/MultiSelectField.tsx
@@ -9,18 +9,13 @@ import { defaultStyles } from './defaultStyles';
 import type { OptionType, SelectFieldProps } from './types';
 
 // Lazy-load react-select on the client to reduce initial bundle size
-const Select = dynamic(() => import('react-select'), {
+const Select = dynamic<
+	ReactSelectProps<OptionType, true, GroupBase<OptionType>>
+>(() => import('react-select'), {
 	ssr: false,
 	// lightweight fallback placeholder with roughly the control height
 	loading: () => <div style={{ height: 38 }} />
 });
-
-// Preserve strong typing for consumers while passing props to the dynamic component
-type SelectProps = ReactSelectProps<OptionType, true, GroupBase<OptionType>>;
-const TypedSelect = (props: SelectProps) => {
-	const Component = Select as unknown as React.ComponentType<any>;
-	return <Component {...(props as any)} />;
-};
 
 export const MultiSelectField = ({
 	options,
@@ -41,7 +36,7 @@ export const MultiSelectField = ({
 	}, [styles]);
 
 	return (
-		<TypedSelect
+		<Select
 			isMulti={true}
 			options={options}
 			value={value}
