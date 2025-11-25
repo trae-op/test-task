458b460fdbbb454457873cb0930434be3802b43a
oleg.pronchenko <traeop@gmail.com>
2025-11-25 14:23:21 +0200

    refactor(api): remove unused GET routes and clean up code

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,9 +1,5 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
-import { type TOrder } from '@/types/orders';
-
-import { calculateOrderTotals } from '@/utils/orders';
-import { parseQueryParams } from '@/utils/routing/routing';
 import { getUserSession } from '@/utils/session';
 
 import { prisma } from '@/prisma/prisma-client';
@@ -74,65 +70,6 @@ export const DELETE = async (req: NextRequest) => {
 			{
 				status: 500
 			}
-		);
-	}
-};
-
-export const GET = async (req: NextRequest) => {
-	try {
-		const userSession = await getUserSession();
-		if (userSession === null) {
-			return NextResponse.json(
-				{ code: 'UNAUTHORIZED', ok: false },
-				{ status: 401 }
-			);
-		}
-
-		const searchParams = req.nextUrl.searchParams;
-		const id = searchParams.get('id');
-		const type = searchParams.get('type');
-		const where: { id?: string; type?: string } = {};
-		if (id) where.id = id;
-		if (type) where.type = type;
-
-		const paramsArr: string[] = [];
-		for (const [key, value] of searchParams.entries()) {
-			if (value !== null && value !== '' && /fields/.test(key)) {
-				paramsArr.push(`${key}=${value}`);
-			}
-		}
-
-		const queryString = paramsArr.join('&');
-		const select = parseQueryParams(queryString);
-
-		const entities = await prisma.order.findMany({
-			...(Object.keys(select).length
-				? {
-						select
-					}
-				: {}),
-			where: {
-				...where,
-				userId: userSession.id
-			}
-		});
-
-		return NextResponse.json(
-			{
-				ok: true,
-				items: calculateOrderTotals(
-					entities.map((item: TOrder) => ({
-						...item,
-						amountOfProducts: item.products ? item.products.length : 0
-					}))
-				)
-			},
-			{ status: 200 }
-		);
-	} catch (error) {
-		return NextResponse.json(
-			{ ok: false, message: 'SERVER_ERROR' },
-			{ status: 500 }
 		);
 	}
 };
